73f6a05744e97eb4b044089d0a439fd39b34fdd8
cctheone <ccevans1@gmail.com>
2016-09-20 10:54:23 -0400

    update dupp image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def thestudio
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/home2'
    get 'pages/home3'
   root 'pages#home'
+   get '/thestudio', to: 'pages#thestudio'
  
 end
