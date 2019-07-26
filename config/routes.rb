58bc40a1fa1f0a01987ed08737f37ab33bb91e3b
CC <ccevans1@gmail.com>
2019-07-26 18:24:10 -0400

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/home2'
-  get 'pages/home3'
-  get 'pages/email'
-
-  get '/thestudio', to: 'pages#thestudio'
-  get '/theconcept', to: 'pages#theconcept'
-  get '/happeningnow', to: 'pages#happeningnow'
-  get '/meetandgreet', to: 'pages#meetandgreet'
-  get '/communityboard', to: 'pages#communityboard'
-  get '/home', to: 'pages#home'
-  get '/rentvenue', to: 'pages#rentvenue'
-  get '/windowshopping', to: 'pages#windowshopping'
-  get '/socialize', to: 'pages#socialize'
+  # get 'pages/home'
+  # get 'pages/home2'
+  # get 'pages/home3'
+  # get 'pages/email'
+  #
+  # get '/thestudio', to: 'pages#thestudio'
+  # get '/theconcept', to: 'pages#theconcept'
+  # get '/happeningnow', to: 'pages#happeningnow'
+  # get '/meetandgreet', to: 'pages#meetandgreet'
+  # get '/communityboard', to: 'pages#communityboard'
+  # get '/home', to: 'pages#home'
+  # get '/rentvenue', to: 'pages#rentvenue'
+  # get '/windowshopping', to: 'pages#windowshopping'
+  # get '/socialize', to: 'pages#socialize'
 
   root 'pages#email'
 end
